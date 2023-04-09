d5ddf583cfb74396f64632d0c9b386c2b80b2857
rileyunverferth <rileyunverferth@gmail.com>
2023-04-08 20:56:21 -0700

    adding data js files

--- logic.js
+++ logic.js
@@ -1,5 +1,13 @@
+var data2012 = fetch("2012_ev_data.json");
+console.log("2012 Data: ", data2012);
 
-mapCenter = [40.233912, -97.595979]
+var data2014 = fetch("2014_ev_data.json").then(response => response.json());
+console.log("2014 Data: ", data2014);
+
+var data2021 = fetch("2021_ev_data.json").then(response => response.json());
+console.log("2021 Data: ", data2021);
+
+mapCenter = [38.147853, -98.612973];
 
 var countryMap = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
@@ -7,11 +15,11 @@ var countryMap = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png
 
   var myMap = L.map("map", {
     center: mapCenter,
-    zoom: 6,
+    zoom: 5,
     layers: countryMap
   });
 
-function createMarkers(data) {
+/*function createMarkers(data) {
   
   var stations = data...
 
@@ -20,4 +28,4 @@ function createMarkers(data) {
   for (var i = 0; i > stations.length; i++)
     var station = stations[i];
     
-}
+}*/
