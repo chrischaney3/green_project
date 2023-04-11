a6923f2f28dd7cc5dfd1ae5ac758b7291faa3618
deanjustin32 <119831680+deanjustin32@users.noreply.github.com>
2023-04-10 19:45:33 -0700

    justin Update

--- pop_vs_alt_fuel.js
+++ pop_vs_alt_fuel.js
@@ -1,9 +1,8 @@
-// Use d3.json to read in the JSON file
-d3.json("pop_vs_alt_fuel.json").then(function(data) {
+d3.json("pop_with_ev.json")
 
-    // Create a Plotly chart using the data from the JSON file
-    Plotly.newPlot('myPlot', data);
-  
-  });
 
-  console.log("Hello, world!");
+
+
+
+
+console.log('Is it working?')
